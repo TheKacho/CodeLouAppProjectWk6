24d724b445702cb8c6f5ad962c7484c45738a1b1
TheKacho <abyssknightzero@gmail.com>
2022-02-12 02:26:22 -0500

    Add project files.

---
+++ CodeLouAppProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CodeLouisvilleLibrary;
+
+namespace CodeLouAppProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool doesThisMakeSense = CodeLouisvilleAppBase.Prompt4YesNo("Does this make sense? ");
+            if (doesThisMakeSense)
+                Console.WriteLine("I've got this!!!"); // previously, it was CodeLouisvilleProjectBase, so VS raised an error
+                                                       // saying prompt4YesNo doesn't exist b/c of it, so it is now CodeLouisvilleAppBase instead
+            else
+                Console.WriteLine("Blame John");
+        }
+    }
+}
